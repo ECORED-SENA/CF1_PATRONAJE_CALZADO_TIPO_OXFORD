9ab30a2241ddeff060fc3f11bcad1e4278f932e7
Cielo Angulo <cielodamarisangulo@gmail.com>
2025-05-30 17:45:00 -0500

    se carga CFA

--- src/config/global.js
+++ src/config/global.js
@@ -187,7 +187,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/52450480_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
